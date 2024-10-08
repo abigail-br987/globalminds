9a866f3c8e23f8d76459fe26f6be5f69fc334c57
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-08 10:51:28 -0500

    init

---
+++ globalminds/src/App.tsx
@@ -0,0 +1,13 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
