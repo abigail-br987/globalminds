ab114e85fb5884edb082327bbf082b63a09ab0a8
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-09 10:26:22 -0500

    add structure and join us

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ import Mission from "../components/Mission";
 import Structure from "../components/Structure";
 import Photo from "../components/Photo";
 import Figures from "../components/Figures";
+import JoinUs from "../components/JoinUs";
 function Home() {
   return (
     <div className="flex flex-col justify-center items-center m-auto bg-gbBlack">
@@ -12,11 +13,12 @@ function Home() {
       <Figures />
       <Landing />      
 
-      <div className="max-w-screen-xl pb-20">
+      <div className="max-w-screen-xl pb-20 p-6">
       <AboutUs />
      <Mission />
-      <Photo/>{/* 
-      <Structure />*/} 
+      <Photo/>
+      <Structure />
+      <JoinUs />
       </div>
     </div>
   );
