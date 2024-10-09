0218c5b150a31b31a8e14796d4377b7dc177a3ad
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-09 09:48:33 -0500

    two photos -> 1 photo

---
+++ src/components/Photo.tsx
@@ -0,0 +1,32 @@
+import Figure11 from "../svg/Figure11";
+import Figure3 from "../svg/Figure3";
+import { motion } from "framer-motion";
+import { animations } from "./animations/animations";
+
+function Photo() {
+  return (
+    <>
+      <motion.div
+        className="mt-10 relative"
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true }}
+        variants={animations.sectionVariant}
+      >
+        <Figure11 color="gbRed" className="absolute h-10 left-1/2 top-2 z-10" />
+        <Figure11
+          color="gbYellow"
+          className="absolute h-10 right-3 top-20 z-10"
+        />
+        <Figure3 className="absolute h-10 left-1/3 bottom-10 z-10" />
+        <img
+          className="object-cover rounded-lg shadow-lg w-full aspect-[16/6]
+              border-gbBlue border-[10px]"
+          src="https://placehold.co/600x400"
+        />
+      </motion.div>
+    </>
+  );
+}
+
+export default Photo;
