d6782bfbbbad75293bfaf701181617533bcb874e
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-09 10:26:02 -0500

    add joinus

---
+++ src/components/JoinUs.tsx
@@ -0,0 +1,17 @@
+function JoinUs() {
+  return (
+    <div className="bg-gbYellow">
+      <h2>Unete</h2>
+      <h3>Unete como Mentee</h3>
+      <h4>¿Qué ofrecemos?</h4>
+      <h4>Responsabilidades</h4>
+
+      <h3>Unete como Mentor</h3>
+      <h3>Unete como Ponente</h3>
+      <h3>Unete como Staff</h3>
+
+    </div>
+  );
+}
+
+export default JoinUs;
