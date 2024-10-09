64ae69925f91beb72c1b3a6731f935ef5385e14a
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-09 09:48:53 -0500

    home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,18 +3,20 @@ import NavBar from "../components/NavBar";
 import AboutUs from "../components/AboutUs";
 import Mission from "../components/Mission";
 import Structure from "../components/Structure";
-import Photos from "../components/Photos";
+import Photo from "../components/Photo";
+import Figures from "../components/Figures";
 function Home() {
   return (
     <div className="flex flex-col justify-center items-center m-auto bg-gbBlack">
       <NavBar />
+      <Figures />
+      <Landing />      
 
-      <Landing />
       <div className="max-w-screen-xl pb-20">
       <AboutUs />
-      <Mission />
-      <Photos/>
-      <Structure />
+     <Mission />
+      <Photo/>{/* 
+      <Structure />*/} 
       </div>
     </div>
   );
