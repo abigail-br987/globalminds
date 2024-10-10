ef7af756427fc5c98e2dc493ffa61de69b650d61
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-10 13:29:37 -0500

    content script

---
+++ src/assets/script/content.tsx
@@ -0,0 +1,141 @@
+import Figure11 from "../../svg/Figure11";
+import Figure12 from "../../svg/Figure12";
+import Figure3 from "../../svg/Figure3";
+export const aboutUsContent = [
+  {
+    title: "¿Quiénes Somos?",
+    description:
+      "Global Minds es una organización juvenil creada para empoderar a jóvenes latinos, proporcionando acceso a recursos, conexiones y mentoría personalizada. Creemos que el éxito individual puede multiplicarse para el beneficio de la comunidad, por lo que ofrecemos asesoría gratuita para apoyar a los estudiantes a lograr sus metas educativas y profesionales en el extranjero.",
+  },
+];
+export const missionContent = [
+  {
+    title: "Nuestra Misión",
+    description:
+      "Nuestra misión es crear una plataforma donde los jóvenes puedan acceder a mentores, charlas inspiradoras y talleres prácticos, eliminando barreras económicas y culturales que limitan las oportunidades de muchos. ",
+  },
+];
+export const joinOptions = [
+  {
+    title: "Únete como Mentee",
+    description:
+      "Si eres un joven latino que busca recursos, mentoría personalizada y oportunidades para acceder a universidades y programas internacionales, Global Minds es para ti.",
+    offerings: [
+      "Acceso a mentoría personalizada.",
+      "Participación en talleres de preparación académica.",
+      "Networking con jóvenes y profesionales de todo el mundo.",
+    ],
+    responsibilities: [
+      "Asistir a las reuniones de mentoría programadas.",
+      "Completar tareas asignadas por tu mentor (investigaciones, aplicaciones, etc.).",
+      "Participar activamente en los talleres y charlas ofrecidas por Global Minds.",
+    ],
+    image: "https://placehold.co/600x400",
+  },
+  {
+    title: "Únete como Mentor",
+    description:
+      "Si eres un profesional o estudiante avanzado que desea guiar a la próxima generación de jóvenes latinos, Global Minds te ofrece la oportunidad de hacer una diferencia.",
+    offerings: [
+      "Oportunidad de impactar vidas con tu conocimiento.",
+      "Contribución al desarrollo de futuros líderes.",
+      "Expansión de tu red profesional con otros mentores y profesionales.",
+    ],
+    responsibilities: [
+      "Realizar reuniones regulares con los mentees asignados.",
+      "Ofrecer apoyo académico y profesional en las áreas que dominas.",
+      "Proveer consejos prácticos y motivación constante.",
+      "Evaluar el progreso del mentee y ofrecer retroalimentación constructiva.",
+    ],
+    image: "https://placehold.co/600x400",
+  },
+  {
+    title: "Únete como Ponente",
+    description:
+      "Si has tenido éxito en tu campo y quieres inspirar a otros jóvenes con tu historia, únete como ponente en nuestras charlas semanales.",
+    offerings: [
+      "Una plataforma para compartir tu historia y conocimiento.",
+      "Conexión con jóvenes motivados y en busca de inspiración.",
+      "Un impacto positivo en la comunidad.",
+    ],
+    responsibilities: [
+      "Preparar una charla motivacional de entre 30 a 45 minutos.",
+      "Responder a las preguntas de los asistentes al final de la charla.",
+      "Participar en las sesiones de networking post-evento para ofrecer más insights.",
+    ],
+    image: "https://placehold.co/600x400",
+  },
+  {
+    title: "Únete como Staff",
+    description:
+      "Si te apasiona la educación, el empoderamiento juvenil y la organización de eventos, Global Minds busca personas comprometidas.",
+    offerings: [
+      "Desarrollo de habilidades en gestión de proyectos.",
+      "Participación activa en la planificación y ejecución de eventos.",
+      "Oportunidades de crecimiento dentro de la organización.",
+    ],
+    responsibilities: [
+      "Asistir en la organización de charlas y eventos.",
+      "Coordinar la logística de los workshops y mentorías.",
+      "Gestionar la comunicación entre mentores, mentees y conferencistas.",
+    ],
+    image: "https://placehold.co/600x400",
+  },
+];
+export const donateMessage = [
+  "¡Haz una diferencia hoy! Tu contribución permite que más jóvenes accedan a mentoría gratuita y oportunidades educativas.",
+  "Garantizamos que tu donación impactará directamente en la vida de jóvenes latinos.",
+  "Te mantendremos informado sobre cómo se usan los fondos para expandir nuestros programas.",
+];
+export const structureItems = [
+  {
+    title: "Inspirar",
+    description:
+      "Charlas semanales con latinos exitosos para inspirar a los estudiantes y ayudarles a definir sus metas.",
+    figure: (
+      <Figure11 color="gbGreen" className="w-10 absolute -rotate-45 z-0" />
+    ),
+  },
+  {
+    title: "Apoyar",
+    description:
+      "Mentoría personalizada para guiar a los estudiantes en sus aplicaciones a concursos, becas y universidades.",
+    figure: (
+      <Figure12 color="gbBlue" className="w-10 absolute -rotate-45 z-0" />
+    ),
+  },
+  {
+    title: "Preparar",
+    description:
+      "Talleres y capacitaciones para fortalecer los currículums y preparar a los estudiantes para actividades relevantes.",
+    figure: <Figure12 color="gbRed" className="w-10 absolute rotate-45 z-0" />,
+  },
+  {
+    title: "Conectar",
+    description:
+      "Crear una comunidad de apoyo y networking entre estudiantes y mentores para fomentar la colaboración y el intercambio de experiencias.",
+    figure: <Figure3 className="w-10 absolute -rotate-45 z-0" />,
+  },
+];
+export const impactData = [
+  {
+    value: "+23",
+    text: "charlas organizadas con expertos latinos",
+  },
+  {
+    value: "+1145",
+    text: "estudiantes impactados, más oportunidades",
+  },
+  {
+    value: "+35",
+    text: "mentores activos de universidades de prestigio",
+  },
+  {
+    value: "75%",
+    text: "mentees admitidos en programas int. o becas",
+  },
+  {
+    value: "50%",
+    text: "crecimiento mensual en la participación",
+  },
+];
