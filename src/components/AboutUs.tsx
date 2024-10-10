50077a80c49e5c979222a21fedeab582c2161d95
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-10 13:29:44 -0500

    use list

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -1,5 +1,7 @@
 import { motion } from "framer-motion";
 import { animations } from "./animations/animations";
+import { aboutUsContent } from "../assets/script/content.tsx";
+
 function AboutUs() {
   return (
     <>
@@ -10,21 +12,18 @@ function AboutUs() {
         viewport={{ once: true }}
         variants={animations.sectionVariant}
       >
-        <div className="bg-gbYellow p-8 md:p-10 rotate space-y-4 m-3 -rotate-1 rounded-lg w-full md:w-1/2">
-          <h2>¿Quiénes Somos?</h2>
-          <p>
-            Global Minds es una organización juvenil creada para empoderar a
-            jóvenes latinos, proporcionando acceso a recursos, conexiones y
-            mentoría personalizada. Creemos que el éxito individual puede
-            multiplicarse para el beneficio de la comunidad, por lo que
-            ofrecemos asesoría gratuita para apoyar a los estudiantes a lograr
-            sus metas educativas y profesionales en el extranjero.
-          </p>
-        </div>
+        {aboutUsContent.map((item, index) => (
+          <div
+            key={index}
+            className="bg-gbYellow p-8 md:p-10 rotate space-y-4 m-3 -rotate-1 rounded-lg w-full md:w-1/2"
+          >
+            <h2>{item.title}</h2>
+            <p>{item.description}</p>
+          </div>
+        ))}
         <div className="relative w-full md:w-1/2 h-64 md:h-96">
           <img
-            className="absolute inset-0 w-full h-full object-cover rounded-lg shadow-lg
-             transform translate-x-0 md:translate-x-8 translate-y-5 md:translate-y-8 rotate-2 md:rotate-4 border-gbYellow border-[10px]"
+            className="absolute inset-0 w-full h-full object-cover rounded-lg shadow-lg transform translate-x-0 md:translate-x-8 translate-y-5 md:translate-y-8 rotate-2 md:rotate-4 border-gbYellow border-[10px]"
             src="https://placehold.co/600x400"
             alt="Description"
           />
