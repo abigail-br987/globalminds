a80c127b4d71260654eba2690d4cf291605a846b
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-10 23:11:44 -0500

    change css

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -6,7 +6,7 @@ function AboutUs() {
   return (
     <>
       <motion.div
-        className="flex flex-col md:flex-row items-center w-full"
+        className="md:grid max-md:space-y-8 max-md:block md:grid-cols-2 md:gap-10"
         initial="offscreen"
         whileInView="onscreen"
         viewport={{ once: true }}
@@ -15,15 +15,15 @@ function AboutUs() {
         {aboutUsContent.map((item, index) => (
           <div
             key={index}
-            className="bg-gbYellow p-8 md:p-10 rotate space-y-4 m-3 -rotate-1 rounded-lg w-full md:w-1/2"
+            className="bg-gbYellow h-max p-8 space-y-4 m-3 -rotate-1 rounded-lg w-full"
           >
             <h2>{item.title}</h2>
             <p>{item.description}</p>
           </div>
         ))}
-        <div className="relative w-full md:w-1/2 h-64 md:h-96">
+        <div className="">
           <img
-            className="absolute inset-0 w-full h-full object-cover rounded-lg shadow-lg transform translate-x-0 md:translate-x-8 translate-y-5 md:translate-y-8 rotate-2 md:rotate-4 border-gbYellow border-[10px]"
+            className="w-full h-full object-cover  rounded-lg rotate-2 border-gbYellow border-[10px]"
             src="https://placehold.co/600x400"
             alt="Description"
           />
