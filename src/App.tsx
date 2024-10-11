47b174d5db442c7b22aea112c50ab417389b8a9f
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-11 09:20:21 -0500

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import Home from "./pages/Home";
-
-function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Nosotros from './pages/Nosotros';
+const App = () => {
   return (
-    <> 
-      <Home  />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/nosotros" element={<Nosotros />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
