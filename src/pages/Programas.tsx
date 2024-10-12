356f7f3c7ff403f5391bf115741ca3d745a8f62b
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-11 21:30:08 -0500

    feat: programas component

---
+++ src/pages/Programas.tsx
@@ -0,0 +1,12 @@
+import NavBar from "../components/NavBar"
+import Footer from "../components/Footer"
+
+function Programas () {
+    return (
+        <div className="flex flex-col justify-center items-center m-auto bg-gbBlack">
+        <NavBar />
+        <Footer />
+        </div>
+    )
+}
+export default Programas
