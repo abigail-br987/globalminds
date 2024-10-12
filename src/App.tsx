369c94e309c3d80636fdbc1ef9525570b2a6c2c0
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-11 21:29:39 -0500

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Nosotros from './pages/Nosotros';
+import Programas from './pages/Programas';
+import Contacto from './pages/Contacto';
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/programas" element={<Programas />} />
         <Route path="/nosotros" element={<Nosotros />} />
+        <Route path="/contacto" element={<Contacto />} />
       </Routes>
     </Router>
   );
