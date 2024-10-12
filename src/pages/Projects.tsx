5691dd189a08c9416b02470f1410667f0e4e9d8d
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-11 21:30:16 -0500

    feat: project component

---
+++ src/pages/Projects.tsx
@@ -0,0 +1,15 @@
+function Project() {
+  return (
+    <>
+      <div className="grid grid-cols-2 gap-4">
+        <div>
+          <img src="" />
+        </div>
+        <div>Name of the Event</div>
+        <p>Description of the event</p>
+      </div>
+    </>
+  );
+}
+
+export default Project;
