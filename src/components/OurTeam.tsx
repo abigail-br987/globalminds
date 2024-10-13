05fec121f1ff73d4d9f36c91338ab2ce413688a6
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-13 12:56:42 -0500

    feat: users display

--- src/components/OurTeam.tsx
+++ src/components/OurTeam.tsx
@@ -1,85 +1,73 @@
 import logo from "/logo/mainlogo.png";
-import { FaSquareFull } from "react-icons/fa6";
 import { useState } from "react";
 import { teamMembers } from "../assets/script/content";
 import { globalMindsColors } from "../assets/script/content";
-import { ourTeamParagraph } from "../assets/script/content";
+import Modal from "./Modal";
 
-function OurTeam() {
+const OurTeam: React.FC = () => {
   const [selectedMember, setSelectedMember] = useState<number | null>(null);
-  return (
-    <div className="bg-gbBlack">
-      <div className="max-w-screen-xl p-10 z-10 flex m-auto">
-        <div className=" md:flex justify-between w-full">
-          <div className="max-md:hidden space-y-3 text-gbWhite pr-10">
-            {ourTeamParagraph.map((paragraph, index) => (
-              <p key={index}>{paragraph}</p>
-            ))}
-            {teamMembers.map((member, index) => {
-              const isSelected = selectedMember === index;
-              const textColor = isSelected
-                ? globalMindsColors[index % globalMindsColors.length]
-                : "gbWhite";
-              return (
-                <div
-                  key={index}
-                  className={`group cursor-pointer transition-all duration-300 ${
-                    isSelected ? "py-3 underline-offset-2 underline" : ""
-                  } opacity-${isSelected ? "100" : "90"} text-${textColor}`}
-                  onMouseEnter={() => setSelectedMember(index)}
-                  onMouseLeave={() => setSelectedMember(null)}
-                  onClick={() => setSelectedMember(index)}
-                >
-                  <h2>{member.name}</h2>
-                  <div className="flex items-center space-x-2">
-                    <FaSquareFull />
-                    <p className="uppercase">{member.position}</p>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
 
-          <div className="max-w-screen-md grid grid-cols-3 h-min gap-6">
-            {teamMembers.map((member, index) => {
-              const isSelected = selectedMember === index;
-              return (
-                <div
-                  key={index}
-                  className={`group relative cursor-pointer ${
-                    isSelected
-                      ? `scale-105 bg-${
-                          globalMindsColors[index % globalMindsColors.length]
-                        } opacity-100`
-                      : "opacity-90"
-                  }`}
-                >
-                  <img
-                    src={member.image || logo}
-                    alt={`Logo for ${member.name}`}
-                    className="w-full h-fit duration-300 transition-all"
-                    onMouseEnter={() => setSelectedMember(index)}
-                    onMouseLeave={() => setSelectedMember(null)}
-                  />
-                  <div
-                    className={`absolute text-sm bottom-0 m-2 border-gbBlack border px-1 bg-gbBlack text-gbWhite transition-all ${
-                      isSelected
-                        ? `bg-${
-                            globalMindsColors[index % globalMindsColors.length]
-                          }`
-                        : ""
-                    }`}
-                  >
-                    {member.name}
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
+  const handleMemberClick = (index: number) => {
+    setSelectedMember(index);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedMember(null);
+  };
+
+  return (
+    <div className="bg-gbBlack max-w-screen-xl my-10 m-auto">
+      <div className="max-w-lg mb-6">
+        <p>Conoce al Equipo detrás de </p>
+        <h2>GlobalMinds</h2>
+        <p className="mt-3">
+          Global Minds es una organización juvenil creada para empoderar a
+          jóvenes latinos, proporcionando acceso a recursos, conexiones y
+          mentoría personalizada.
+        </p>
       </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 h-min gap-6">
+        {teamMembers.map((member, index) => {
+          return (
+            <div
+              key={index}
+              onClick={() => handleMemberClick(index)}
+              className={`group relative cursor-pointer opacity-90 transition-transform duration-300 hover:scale-105 hover:opacity-100`}
+            >
+              <img
+                src={logo}
+                alt={`Logo for ${member.name}`}
+                className="w-full h-fit duration-300 transition-all"
+              />
+              <div
+                className={`absolute text-sm bottom-0 m-2 border-gbBlack border px-1 bg-gbBlack text-gbWhite transition-all 
+                }`}
+              >
+                <h3>{member.name}</h3>
+                <p>{member.position}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      <Modal isOpen={selectedMember !== null} onClose={handleCloseModal}>
+        {selectedMember !== null && (
+          <div className="text-center">
+            <h2 className="text-lg font-bold">
+              {teamMembers[selectedMember].name}
+            </h2>
+            <img
+              src={logo}
+              alt={teamMembers[selectedMember].name}
+              className="w-32 h-32 mx-auto rounded-full"
+            />
+            <p className="mt-2">{teamMembers[selectedMember].description}</p>
+          </div>
+        )}
+      </Modal>
     </div>
   );
-}
+};
 
 export default OurTeam;
