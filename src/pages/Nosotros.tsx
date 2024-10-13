9dffc9ce587e36779f30afc7faeaece3bd698fc0
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-13 12:57:10 -0500

    feat styles

--- src/pages/Nosotros.tsx
+++ src/pages/Nosotros.tsx
@@ -2,15 +2,22 @@ import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import OurTeam from "../components/OurTeam";
 import FollowUs from "../components/FollowUs";
+import banner from "/images/banner.png"
+
 function Nosotros() {
 
   return (
     <div className="bg-gbBlack">
-      <NavBar />
-      <div className="max-w-screen-xl p-10 z-10 flex flex-col items-center justify-center m-auto">
-        <h1 className="max-lg:text-5xl">NUESTRO EQUIPO</h1>
-        <OurTeam/>
+       <div
+        className="w-full h-64 bg-cover bg-center flex justify-center items-center"
+        style={{ backgroundImage: `url(${banner})` }}
+      >
+        <h1 style={{ textShadow: "3px 3px 2px rgba(2, 2, 2, 2)" }} className="">
+          NUESTRO EQUIPO
+        </h1>
       </div>
+      <NavBar />
+      <OurTeam/>
       <FollowUs/>
       <Footer />
     </div>
