3d0ff44134e779041f1396f85ad842d96181d342
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-13 12:57:04 -0500

    styles text shadow

--- src/pages/Contacto.tsx
+++ src/pages/Contacto.tsx
@@ -1,23 +1,26 @@
-import NavBar from "../components/NavBar"
-import Contact from "../components/Contact"
-import Footer from "../components/Footer"
-import banner from "/images/banner.png"
+import NavBar from "../components/NavBar";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+import banner from "/images/banner.png";
+
 const Contacto = () => {
-    return (
-        <div className="flex flex-col justify-center items-center min-h-screen bg-gbBlack text-white">
-            <div 
-                className="w-full h-64 bg-cover bg-center flex justify-center items-center" 
-                style={{ backgroundImage: `url(${banner})` }}
-            >
-                <h1>CONTÁCTANOS</h1>
-            </div>
-            <div className="max-w-screen-xl p-10 z-10 space-y-20">
-            <Contact />
-            </div>
-            <NavBar />
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen bg-gbBlack text-white">
+      <div
+        className="w-full h-64 bg-cover bg-center flex justify-center items-center"
+        style={{ backgroundImage: `url(${banner})` }}
+      >
+        <h1 style={{ textShadow: "3px 3px 2px rgba(2, 2, 2, 2)" }} className="">
+          CONTÁCTANOS
+        </h1>
+      </div>
+      <div className="max-w-screen-xl p-10 z-10 space-y-20">
+        <Contact />
+      </div>
+      <NavBar />
+      <Footer />
+    </div>
+  );
 };
 
 export default Contacto;
