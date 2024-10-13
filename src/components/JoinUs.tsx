95690c2976f1361eeb970e956c989c75afb889e6
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-13 12:55:56 -0500

    prettier

--- src/components/JoinUs.tsx
+++ src/components/JoinUs.tsx
@@ -1,85 +1,77 @@
 import Dropdown from "./small_components/Dropdown";
 import { joinOptions } from "../assets/script/content.tsx";
 import { useState } from "react";
+import ButtonWithArrow from "./small_components/ButtonWithArrow.tsx";
+import Modal from "./Modal.tsx";
+import { globalMindsColors } from "../assets/script/content.tsx";
+interface JoinOption {
+  title: string;
+  icon: JSX.Element; // Assuming icon is a JSX element
+  description: string;
+  offerings: string[];
+  responsibilities: string[];
+  image: string;
+}
 
-type DropdownState = { [key: number]: boolean };
-const getBackgroundColor = (index: number) => {
-  const colors = ["bg-gbRed", "bg-gbYellow", "bg-gbBlue", "bg-gbGreen"];
-  return colors[index % colors.length];
-};
+const JoinUs: React.FC = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState<JoinOption | null>(null);
 
-function JoinUs() {
-  const [dropdownOpen, setDropdownOpen] = useState<DropdownState>({});
-
-  const toggleDropdown = (index: number) => {
-    setDropdownOpen((prev: DropdownState) => ({
-      ...prev,
-      [index]: !prev[index],
-    }));
+  const toggleModal = (option: JoinOption | null) => {
+    setSelectedOption(option);
+    setModalOpen(!modalOpen);
   };
 
   return (
     <div>
       <h2 className="text-center text-gbWhite">Únete</h2>
       <div className="mt-6 mb-10 space-y-4 rounded text-gbWhite hyphens-auto">
-        <div className="sm:grid  max-sm:space-y-8 max-sm:block grid-cols-2 gap-10">
+        <div className="sm:grid max-sm:space-y-8 max-sm:block grid-cols-2 gap-10">
           {joinOptions.map((option, index) => (
             <div
               key={index}
-              className={`sm:flex  text-gbBlack ${getBackgroundColor(index)} p-4 rounded-lg`}
+              className={`sm:flex text-gbBlack p-4 rounded-lg bg-${
+                globalMindsColors[index % globalMindsColors.length]
+              }`}
             >
               <div className={"flex-1"}>
-                <h3>{option.title}</h3>
-                <p>{option.description}</p>
-                <Dropdown
-                  name={
-                    <button
-                      className="bg-white text-gbBlack font-semibold min-w-60"
-                      onClick={() => toggleDropdown(index)}
-                    >
-                      ¿Qué ofrecemos?
-                    </button>
-                  }
-                  options={
-                    <ul>
-                      {option.offerings.map((offering, i) => (
-                        <li key={i}>- {offering}</li>
-                      ))}
-                    </ul>
-                  }
-                />
-                <Dropdown
-                  name={
-                    <button
-                      className="bg-white text-gbBlack font-semibold min-w-60"
-                      onClick={() => toggleDropdown(index)}
-                    >
-                      Responsabilidades
-                    </button>
-                  }
-                  options={
-                    <ul>
-                      {option.responsibilities.map((responsibility, i) => (
-                        <li key={i}>- {responsibility}</li>
-                      ))}
-                    </ul>
-                  }
-                />
-              </div>
-              {!dropdownOpen[index] && (
-                <div className="flex-shrink max-lg:hidden pl-6">
-                  <img
-                    src={option.image}
-                    className="w-full h-full object-cover"
-                  />
+                <div className="flex items-center">
+                  {option.icon} {". "}
+                  <h3 className="">{option.title}</h3>
                 </div>
-              )}
+                <p>{option.description}</p>
+                <ButtonWithArrow
+                  className="bg-gbWhite"
+                  onClick={() => toggleModal(option)}
+                >
+                  Más información
+                </ButtonWithArrow>
+              </div>
             </div>
           ))}
         </div>
       </div>
+      <Modal isOpen={modalOpen} onClose={() => toggleModal(null)}>
+        {selectedOption && (
+          <>
+            <h3 className="text-lg font-bold">{selectedOption.title}</h3>
+            <h4 className="mt-2 font-semibold">¿Qué ofrecemos?</h4>
+            <ul>
+              {selectedOption.offerings.map((offering, i) => (
+                <li key={i}>- {offering}</li>
+              ))}
+            </ul>
+            <h4 className="mt-2 font-semibold">Responsabilidades</h4>
+            <ul>
+              {selectedOption.responsibilities.map((responsibility, i) => (
+                <li key={i}>- {responsibility}</li>
+              ))}
+            </ul>
+          </>
+        )}
+      </Modal>
     </div>
   );
-}
+};
 
 export default JoinUs;
