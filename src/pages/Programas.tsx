ae4e70e6a0cb0cedc7a21cf300caf002dc9960a8
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-13 12:57:16 -0500

    add oportunidades

--- src/pages/Programas.tsx
+++ src/pages/Programas.tsx
@@ -1,12 +1,22 @@
-import NavBar from "../components/NavBar"
-import Footer from "../components/Footer"
-
-function Programas () {
-    return (
-        <div className="flex flex-col justify-center items-center m-auto bg-gbBlack">
-        <NavBar />
-        <Footer />
-        </div>
-    )
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import DetailedStructure from "../components/DetailedStructure";
+import banner from "/images/banner.png";
+function Programas() {
+  return (
+    <div className="flex flex-col justify-center items-center m-auto bg-gbBlack">
+      <div
+        className="w-full h-64 bg-cover bg-center flex justify-center items-center"
+        style={{ backgroundImage: `url(${banner})` }}
+      >
+        <h1 style={{ textShadow: "3px 3px 2px rgba(2, 2, 2, 2)" }} className="">
+          OPORTUNIDADES
+        </h1>
+      </div>
+      <NavBar />
+      <DetailedStructure />
+      <Footer />
+    </div>
+  );
 }
-export default Programas
+export default Programas;
