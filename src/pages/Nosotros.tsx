583d391b7916b416f1ad692d05f5e5bd4f706063
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-13 15:54:44 -0500

    refactor: header

--- src/pages/Nosotros.tsx
+++ src/pages/Nosotros.tsx
@@ -2,23 +2,15 @@ import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import OurTeam from "../components/OurTeam";
 import FollowUs from "../components/FollowUs";
-import banner from "/images/banner.png"
-
+import banner from "/images/banner.png";
+import Header from "../components/small_components/Header";
 function Nosotros() {
-
   return (
     <div className="bg-gbBlack">
-       <div
-        className="w-full h-64 bg-cover bg-center flex justify-center items-center"
-        style={{ backgroundImage: `url(${banner})` }}
-      >
-        <h1 style={{ textShadow: "3px 3px 2px rgba(2, 2, 2, 2)" }} className="">
-          NUESTRO EQUIPO
-        </h1>
-      </div>
+      <Header image={banner}>NUESTRO EQUIPO</Header>
       <NavBar />
-      <OurTeam/>
-      <FollowUs/>
+      <OurTeam />
+      <FollowUs />
       <Footer />
     </div>
   );
