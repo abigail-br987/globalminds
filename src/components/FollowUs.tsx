bc6f6fbfca4ac53e47697d8430bb75896212e170
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-13 08:19:25 -0500

    feat: FollowUs

--- src/components/FollowUs.tsx
+++ src/components/FollowUs.tsx
@@ -1,44 +1,7 @@
-import {
-  FaLinkedin,
-  FaInstagram,
-  FaDiscord,
-  FaWhatsapp,
-  FaEnvelope,
-} from "react-icons/fa";
+
 import banner from "/images/banner.png";
+import { socialLinks } from "../assets/script/content";
 const FollowUs = () => {
-  const links = [
-    {
-      href: "https://www.linkedin.com",
-      bgColor: "bg-gbBlue",
-      text: "Síguenos en LinkedIn",
-      icon: <FaLinkedin />,
-    },
-    {
-      href: "https://www.instagram.com",
-      bgColor: "bg-gsPink",
-      text: "Síguenos en Instagram",
-      icon: <FaInstagram />,
-    },
-    {
-      href: "https://discord.com",
-      bgColor: "bg-gsLightPurple",
-      text: "Únete a nuestro Discord",
-      icon: <FaDiscord />,
-    },
-    {
-      href: "https://www.whatsapp.com",
-      bgColor: "bg-gbGreen",
-      text: "Únete a nuestro grupo de WhatsApp",
-      icon: <FaWhatsapp />,
-    },
-    {
-      href: "mailto:info@.com",
-      bgColor: "bg-gbYellow",
-      text: "Escríbenos por Email",
-      icon: <FaEnvelope />,
-    },
-  ];
 
   return (
     <div
@@ -48,7 +11,7 @@ const FollowUs = () => {
       <div className="p-8 bg-gbBlack rounded-lg shadow-lg text-center max-w-lg space-y-6">
         <h2>¡Síguenos y Únete a Nuestra Comunidad!</h2>
         <div className="flex flex-col">
-          {links.map((link, index) => (
+          {socialLinks.map((link, index) => (
             <a
               key={index}
               href={link.href}
