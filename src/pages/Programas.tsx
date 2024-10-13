dfb34ba9045c39961cca1bc4d17f1f6102329311
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-13 15:54:49 -0500

    refactor: header

--- src/pages/Programas.tsx
+++ src/pages/Programas.tsx
@@ -2,17 +2,11 @@ import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import DetailedStructure from "../components/DetailedStructure";
 import banner from "/images/banner.png";
+import Header from "../components/small_components/Header";
 function Programas() {
   return (
     <div className="flex flex-col justify-center items-center m-auto bg-gbBlack">
-      <div
-        className="w-full h-64 bg-cover bg-center flex justify-center items-center"
-        style={{ backgroundImage: `url(${banner})` }}
-      >
-        <h1 style={{ textShadow: "3px 3px 2px rgba(2, 2, 2, 2)" }} className="">
-          OPORTUNIDADES
-        </h1>
-      </div>
+      <Header image={banner}>OPORTUNIDADES</Header>
       <NavBar />
       <DetailedStructure />
       <Footer />
