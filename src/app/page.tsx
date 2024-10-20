2f068e2720cab2a2edc72a10d2e0aace9d0e6a3e
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-20 11:51:02 -0500

    ii

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,36 @@
-import Link from "next/link";
-import { type SanityDocument } from "next-sanity";
+import Landing from "@/components/Landing";
+import NavBar from "@/components/NavBar";
+import Structure from "@/components/Structure";
+import Photos from "@/components/Photos";
+import Figures from "@/components/Figures";
+import JoinUs from "@/components/JoinUs";
+import Donate from "@/components/Donate";
+import Impact from "@/components/Impact";
+import Footer from "@/components/Footer";
+import Presentation from "@/components/Presentation";
 
-import { client } from "@/sanity/client";
-
-const POSTS_QUERY = `*[
-  _type == "post"
-  && defined(slug.current)
-]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt}`;
-
-const options = { next: { revalidate: 30 } };
-
-export default async function IndexPage() {
-  const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
-
+function Home() {
   return (
-    <div className="container mx-auto min-h-screen max-w-3xl p-8 text-black">
-      <h1 className="text-4xl font-bold mb-8">Posts!</h1>
-      <ul className="flex flex-col gap-y-4">
-        {posts.map((post) => (
-          <li className="hover:underline" key={post._id}>
-            <Link href={`/${post.slug.current}`}>
-              <h2 className="text-xl font-semibold">{post.title}</h2>
-              <p>{new Date(post.publishedAt).toLocaleDateString()}</p>
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <div className="flex flex-col justify-center items-center m-auto bg-gbBlack">
+      <NavBar />
+      <Figures />
+      <Landing />
+    <div className="max-w-screen-xl p-10 z-10 space-y-10">
+       <Presentation/>
+       <Photos
+          images={[
+            "https://placehold.co/600x400",
+            "https://placehold.co/700x400",
+          ]}
+        /> 
+        <Structure /> 
+        <Impact />
+        <JoinUs /> 
+        <Donate />
+      </div> 
+      <Footer />
     </div>
   );
 }
+
+export default Home;
