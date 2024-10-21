38592cf667a6e155190f29de607d38caf1b1a84c
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-20 22:13:55 -0500

    feat queries

--- src/components/DetailedStructure.tsx
+++ src/components/DetailedStructure.tsx
@@ -1,6 +1,6 @@
 import ProgramasComponent from "./Charlas";
 import { client } from "@/sanity/client";
-import { type SanityDocument } from "next-sanity"; 
+import { type SanityDocument } from "next-sanity";
 import ColorfulTitle from "./small_components/ColorfulTitle";
 
 const POSTS_QUERY = `*[ 
@@ -21,40 +21,46 @@ const POSTS_QUERY = `*[
 const options = { next: { revalidate: 30 } };
 
 export default async function DetailedStructure() {
-  const elements = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
+  const elements = await client.fetch<SanityDocument[]>(
+    POSTS_QUERY,
+    {},
+    options
+  );
 
-  const groupedElements = elements.reduce((acc, element) => {
-    const { type } = element;
-    if (!acc[type]) {
-      acc[type] = [];
-    }
-    acc[type].push(element);
-    return acc;
-  }, {} as Record<string, SanityDocument[]>);
+  const groupedElements = elements.reduce(
+    (acc, element) => {
+      const { type } = element;
+      if (!acc[type]) {
+        acc[type] = [];
+      }
+      acc[type].push(element);
+      return acc;
+    },
+    {} as Record<string, SanityDocument[]>
+  );
 
   return (
     <div className="container mx-auto p-4 space-y-6 text-gbBlack">
       {Object.keys(groupedElements).map((type) => (
         <div key={type} className="space-y-4">
+          {type === "charlas" && (
+            <>
+              <ColorfulTitle text="CHARLAS" />
+            </>
+          )}
+          {type === "networking" && <ColorfulTitle text="NETWORKING" />}
+          {type === "talleres" && <ColorfulTitle text="TALLERES" />}
+          {type === "mentores" && <ColorfulTitle text="MENTORES" />}
 
-          {type === "charlas" && (<> 
-            <ColorfulTitle text="CHARLAS"/></>
-          )}
-          {type === "networking" && (
-            <ColorfulTitle text="NETWORKING"/>
-          )}
-          {type === "talleres" && (
-            <ColorfulTitle text="TALLERES"/>
-          )}
-          {type === "mentores" && (
-            <ColorfulTitle text="MENTORES"/>
-          )}
-  
           <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
             {groupedElements[type].map((element) => (
               <div key={element._id} className="border rounded-lg">
                 <ProgramasComponent
-                  photo={element.image ? element.image._upload.previewImage : null}
+                  photo={
+                    element.image && element.image._upload
+                      ? element.image._upload.previewImage
+                      : null
+                  }
                   emoji={element.emoji}
                   title={element.title}
                   date={element.time}
@@ -72,4 +78,5 @@ export default async function DetailedStructure() {
         </div>
       ))}
     </div>
-  )}
+  );
+}
