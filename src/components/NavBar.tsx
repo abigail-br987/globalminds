757971498a0c08966380ab6db60f7d01ba539602
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-21 17:21:59 -0500

    style: navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -13,13 +13,15 @@ function NavBar() {
     <nav className="p-2 fixed w-full top-0 z-50 font-mono">
       <ul className="items-start flex justify-between">
         <Link href={"/home"} className="cursor-pointer flex space-x-3">
+          
+            <div className="py-1 px-3 bg-gbBlack h-max rounded-md space-x-3 font-mono font-bold flex items-center">
             <img
               src={"/logo/mainlogo.png"}
               alt="Home Logo"
-              className="max-w-12 max-h-12 rounded-full"
-            ></img>
-            <div className="py-1 px-3 bg-gbBlack h-max rounded-md font-mono font-bold">
-              GLOBAL MINDS
+              className="max-w-5 max-h-5 rounded-full"
+            ></img> .
+              <span>
+              GLOBAL MINDS</span>
             </div>
         </Link>
         <div className="flex space-x-2 md:space-x-4 py-1 bg-gbBlack z-0 rounded-md px-3">
