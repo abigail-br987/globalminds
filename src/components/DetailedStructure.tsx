497f680fb836f8ef76915dbb79bd8236c420d192
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-21 07:43:58 -0500

    feat: images url

--- src/components/DetailedStructure.tsx
+++ src/components/DetailedStructure.tsx
@@ -1,7 +1,16 @@
 import ProgramasComponent from "./Charlas";
-import { client } from "@/sanity/client";
 import { type SanityDocument } from "next-sanity";
 import ColorfulTitle from "./small_components/ColorfulTitle";
+import imageUrlBuilder from "@sanity/image-url";
+import { client } from "@/sanity/client";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+
+const builder = imageUrlBuilder(client);
+
+// Ensure urlFor returns a string or undefined
+function urlFor(source: SanityImageSource | null): string | undefined {
+  return source ? builder.image(source).url() : undefined; // Use .url() to get a string URL
+}
 
 const POSTS_QUERY = `*[ 
   _type == "offerings" 
@@ -21,12 +30,9 @@ const POSTS_QUERY = `*[
 const options = { next: { revalidate: 30 } };
 
 export default async function DetailedStructure() {
-  const elements = await client.fetch<SanityDocument[]>(
-    POSTS_QUERY,
-    {},
-    options
-  );
+  const elements = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
 
+  // Group elements by type
   const groupedElements = elements.reduce(
     (acc, element) => {
       const { type } = element;
@@ -40,40 +46,33 @@ export default async function DetailedStructure() {
   );
 
   return (
-    <div className="container mx-auto p-4 space-y-6 text-gbBlack">
+    <div className="container mx-auto p-4 space-y-6 text-gbBlack ">
       {Object.keys(groupedElements).map((type) => (
         <div key={type} className="space-y-4">
-          {type === "charlas" && (
-            <>
-              <ColorfulTitle text="CHARLAS" />
-            </>
-          )}
-          {type === "networking" && <ColorfulTitle text="NETWORKING" />}
-          {type === "talleres" && <ColorfulTitle text="TALLERES" />}
-          {type === "mentores" && <ColorfulTitle text="MENTORES" />}
+          <ColorfulTitle text={type.toUpperCase()} />
 
-          <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-            {groupedElements[type].map((element) => (
-              <div key={element._id} className="border rounded-lg">
-                <ProgramasComponent
-                  photo={
-                    element.image && element.image._upload
-                      ? element.image._upload.previewImage
-                      : null
-                  }
-                  emoji={element.emoji}
-                  title={element.title}
-                  date={element.time}
-                  mode={element.mode}
-                  type={element.type}
-                  sociallinks={element.sociallinks}
-                  color={element.color}
-                  description={element.description}
-                  linkName={element.linkName}
-                  url={element.url}
-                />
-              </div>
-            ))}
+          <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4 ">
+            {groupedElements[type].map((element) => {
+              const componentProps = {
+                photo: element.image && element.image.asset ? urlFor(element.image.asset) : undefined,
+                emoji: element.emoji,
+                title: element.title,
+                date: element.time,
+                mode: element.mode,
+                type: element.type,
+                sociallinks: element.sociallinks,
+                color: element.color,
+                description: element.description,
+                linkName: element.linkName,
+                url: element.url,
+              };
+
+              return (
+                <div key={element._id} className="">
+                  <ProgramasComponent {...componentProps} />
+                </div>
+              );
+            })}
           </div>
         </div>
       ))}
