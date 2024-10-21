5401f09da84329b68ee3b94b956f2fbaf94ec890
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-21 08:21:08 -0500

    feat: fetch data

--- src/app/nosotros/page.tsx
+++ src/app/nosotros/page.tsx
@@ -1,21 +1,19 @@
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
-import OurTeam from "@/components/OurTeam";
+import OurTeamServer from "@/components/OurTeamServer"; 
 import FollowUs from "@/components/FollowUs";
 import Header from "@/components/small_components/Header";
 
 const banner = "https://media.licdn.com/dms/image/C4D12AQFS2QZ8phBvvw/article-cover_image-shrink_600_2000/0/1590074321257?e=2147483647&v=beta&t=lAnXk5kw_JqvOx3dg93X_zIcz7TTEnkNpRyqhYAIQ8Q";
 
-function Nosotros() {
+export default async function Nosotros() {
   return (
     <div className="bg-gbBlack">
-      <Header image={banner}></Header>
+      <Header image={banner} />
       <NavBar />
-      <OurTeam />
+      <OurTeamServer />
       <FollowUs />
       <Footer />
     </div>
   );
 }
-
-export default Nosotros;
