77f99d78db89939265c8cbb3e8f02d7593cc87ef
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-21 17:21:13 -0500

    feat: img to Image

--- src/components/Donate.tsx
+++ src/components/Donate.tsx
@@ -1,10 +1,12 @@
 import { donateMessage } from "@/script/content";
 import ButtonWithArrow from "./small_components/ButtonWithArrow";
+import Image from "next/image";
 function Donate() {
   return (
     <div className="flex text-gbWhite items-center">
       <div className="flex-shrink-0 basis-1/6 max-lg:hidden">
-        <img  src={"/logo/mainlogo.png"} alt="image" className="w-full h-full object-cover" />
+        <Image  src={"/logo/mainlogo.png"} alt="image"  layout="responsive" width={100} height={100}
+       objectFit="cover" className="w-full h-full object-cover" />
       </div>
       <div className="lg:pl-6 flex-shrink">
         <ul>
