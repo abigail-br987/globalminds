50e2c6cc5149aac9e91c96ceedebd22eefdd1419
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-21 17:19:40 -0500

    style feat: blog

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -4,6 +4,7 @@ import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 import { client } from "@/sanity/client";
 import Link from "next/link";
 import Image from "next/image";
+
 const POST_QUERY = `*[_type == "post" && slug.current == $slug][0]`;
 
 const { projectId, dataset } = client.config();
@@ -11,6 +12,7 @@ const urlFor = (source: SanityImageSource) =>
   projectId && dataset
     ? imageUrlBuilder({ projectId, dataset }).image(source)
     : null;
+
 const options = { next: { revalidate: 30 } };
 
 export default async function PostPage({
@@ -19,30 +21,35 @@ export default async function PostPage({
   params: { slug: string };
 }) {
   const post = await client.fetch<SanityDocument>(POST_QUERY, params, options);
-  const postImageUrl = post.image
-    ? urlFor(post.image)?.width(550).height(310).url()
+  if (!post) {
+    return <div>Post not found.</div>;
+  }
+  const postImageUrl = post.image1
+    ? urlFor(post.image1)?.width(550).height(310).url()
     : null;
 
   return (
-    <main className="container mx-auto min-h-screen max-w-3xl p-8 flex text-black flex-col gap-4">
+    <main className="container mx-auto min-h-screen max-w-3xl p-8 flex flex-col gap-4 text-gbWhite">
       <Link href="/blog" className="hover:underline">
-        ← Back to posts
+        ← Regresar
       </Link>
       {postImageUrl && (
         <Image
           src={postImageUrl}
           alt={post.title}
-          className="aspect-video rounded-xl"
-          width="550"
-          height="310"
+          className="aspect-video rounded-lg"
+          width={550}
+          height={310}
+          priority
         />
       )}
-      <h1 className="text-4xl font-bold mb-8">{post.title}</h1>
-      <div className="prose">
-        <p>Published: {new Date(post.publishedAt).toLocaleDateString()}</p>
+      <h2 className="break-all">{post.title}</h2>
+      <div className="max-w-full break-all">
+        <p className="break-all">
+          Publicado: {new Date(post.publishedAt).toLocaleDateString()}
+        </p>
         {Array.isArray(post.body) && <PortableText value={post.body} />}
       </div>
     </main>
   );
 }
-
