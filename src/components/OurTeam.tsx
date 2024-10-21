9f6a5a04b41c58c4e9716e3d9a0806c962e7ed93
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-21 07:57:12 -0500

    debug

--- src/components/OurTeam.tsx
+++ src/components/OurTeam.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState } from "react";
-import { teamMembers } from "@/script/content";
 import Modal from "./Modal";
 import Image from "next/image";
 import { SanityDocument } from "next-sanity";
