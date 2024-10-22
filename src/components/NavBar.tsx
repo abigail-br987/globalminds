d7060b996328c598307a942160d0850f389398c0
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-22 16:34:13 -0500

    style: links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -13,30 +13,28 @@ function NavBar() {
     <nav className="p-2 fixed w-full top-0 z-50 font-mono">
       <ul className="items-start flex justify-between">
         <Link href={"/home"} className="cursor-pointer flex space-x-3">
-          
-            <div className="py-1 px-3 bg-gbBlack h-max rounded-md space-x-3 font-mono font-bold flex items-center">
+          <div className="py-1 px-3 bg-gbBlack h-max rounded-md space-x-3 font-mono font-bold flex items-center">
             <img
               src={"/logo/mainlogo.png"}
               alt="Home Logo"
               className="max-w-5 max-h-5 rounded-full"
-            ></img> .
-              <span>
-              GLOBAL MINDS</span>
-            </div>
+            ></img>{" "}
+            .<span>GLOBAL MINDS</span>
+          </div>
         </Link>
         <div className="flex space-x-2 md:space-x-4 py-1 bg-gbBlack z-0 rounded-md px-3">
           {navItems.map((item, index) => (
-            <li
-              key={index}
-              className="relative uppercase cursor-pointer text-gbWhite font-bold group"
-            >
-              <Link href={item.to}>
+            <Link href={item.to}>
+              <li
+                key={index}
+                className="relative uppercase cursor-pointer text-gbWhite font-bold group"
+              >
                 {item.name}
                 <span
                   className={`absolute left-0 rounded-sm -bottom-1 h-1.5 w-full ${item.style} -z-10 transition-all duration-300 transform origin-bottom group-hover:h-8`}
                 ></span>
-              </Link>
-            </li>
+              </li>{" "}
+            </Link>
           ))}
         </div>
       </ul>
