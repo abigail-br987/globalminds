175f1dee00153189e8d5e2c843bb5040d7d10ded
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-22 15:24:37 -0500

    feat: interfaces

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,76 +1,86 @@
-import { MdDateRange } from 'react-icons/md';
-import { FaRegClock } from 'react-icons/fa';
-import Link from 'next/link';
+import { MdDateRange } from "react-icons/md";
+import { FaRegClock } from "react-icons/fa";
+
+import Image from "next/image";
+import Link from "next/link";
+import { TbClockHour4 } from "react-icons/tb";
+import { getIconByType } from "@/app/lib/utils";
+interface FormattedDate {
+  day: string;
+  month: string;
+  hour: string;
+  minutes: string;
+}
+
 interface BlogCardProps {
-    images: string[];
-    date: string;
-    title: string;
-    articleType: string;
-    readTime: string;
-    description: string;
-    slug:string;
-    id:string;
-    onClick?: () => void; 
-    className?: string; 
-  }
+  images: string[];
+  formattedDate?: FormattedDate;
+  title: string;
+  articleType: string;
+  readTime: string;
+  description: string;
+  slug: string;
+  id: string;
+  onClick?: () => void;
+  className?: string;
+}
 
-  const BlogCard: React.FC<BlogCardProps> = ({
-    images,
-    date,
-    title,
-    articleType,
-    readTime,
-    id,
-    slug,
-    description,
-    onClick, 
-    className, 
-  }) => {
-    if (!images.length) {
-      throw new Error('At least one image is required.');
-    }
-  
-    return (
-
-      <div
-        className={`relative ${className}`}
-      >
-          <Link key={id} href={`/blog/${slug}`}> 
-        <div className="w-full h-3/5">
-          <img
+const BlogCard: React.FC<BlogCardProps> = ({
+  images,
+  formattedDate,
+  title,
+  articleType,
+  readTime,
+  id,
+  slug,
+  description,
+  onClick,
+  className,
+}) => {
+  return (
+    <div className={`relative ${className}`}>
+      <Link key={id} href={`/blog/${slug}`}>
+        <figure className="w-full h-3/5 overflow-hidden relative">
+          <Image
             src={images[0]}
-            className="w-full h-full p-2 rounded-lg object-cover"
+            alt="First Image"
+            layout="fill"
+            className="object-cover p-1 rounded-2xl"
           />
-        </div>
-  
+        </figure>
         {images.length > 1 && (
-          <div className="w-full h-2/5">
-            <img
+          <figure className="w-full h-2/5 rounded-2xl overflow-hidden relative">
+            <Image
               src={images[1]}
-              className="w-full h-full p-2 rounded-lg object-cover"
+              alt="Second Image"
+              layout="fill"
+              className="object-cover p-1 rounded-2xl"
             />
-          </div>
+          </figure>
         )}
-  
-        <div className="bg-gbWhite rounded-lg max-w-64 p-6 bottom-0 right-0 absolute text-gbBlack">
-          <h5 className="uppercase opacity-70 text-xs font-semibold flex items-center ">
-            <MdDateRange />
-            {date}
-          </h5>
-          <h4 className="text-lg leading-4 mb-3 mt-2">
-            {title}
-          </h4>
-          <h5 className="uppercase text-xs font-semibold opacity-70 w-full flex justify-between">
-            <span>{articleType}</span>
-            <span className="flex items-center">
-              <FaRegClock />. {readTime}
+        <div className="bg-gbWhite rounded-2xl space-y-2 max-w-72 p-6 bottom-0 right-0 absolute text-gbBlack">
+          <h3 className="pb-1">{title}</h3>
+          <h5 className="text-xs opacity-70 w-full flex justify-between">
+            <span className="flex items-center space-x-1">
+              {getIconByType(articleType)}
+              <span>{articleType}</span>
             </span>
+
+            {formattedDate && (
+              <div className={`flex items-center space-x-1`}>
+                <MdDateRange />
+                <span>
+                  {formattedDate.month} {formattedDate.day}
+                </span>
+              </div>
+            )}
           </h5>
-          <hr className="border-t border-gbBlack my-2" />
+          <hr className="border-t border-gbBlack" />
           <p className="text-xs">{description}</p>
-        </div></Link>
-      </div>
-    );
-  };
-  
-  export default BlogCard
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default BlogCard;
