064498fba990f00340fa8b1830ebb0318f7b7334
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-22 15:24:22 -0500

    feat: utils: date, icons

---
+++ src/app/lib/utils.tsx
@@ -0,0 +1,31 @@
+
+import { RiArticleLine } from "react-icons/ri";
+import { FaHeart } from "react-icons/fa";
+import { FaRegNewspaper } from "react-icons/fa6";
+import { IoDocumentTextOutline } from "react-icons/io5";
+
+
+export const formatDate = (isoDate: string) => {
+  const dateObject = new Date(isoDate);
+  const day = dateObject.getDate().toString(); 
+  const fullMonth = dateObject.toLocaleString("default", { month: "long" });
+  const month = fullMonth.slice(0, 3); 
+  const hour = dateObject.getHours().toString();
+  const minutes = dateObject.getMinutes().toString().padStart(2, "0");
+
+  return { day, month, hour, minutes };
+};
+
+
+export const getIconByType = (type: string) => {
+  switch (type) {
+    case "Artículo":
+      return <RiArticleLine />;
+    case "Testimonio":
+      return <FaHeart />;
+    case "Noticias":
+      return <FaRegNewspaper />;
+    default:
+      return <IoDocumentTextOutline />;
+  }
+};
