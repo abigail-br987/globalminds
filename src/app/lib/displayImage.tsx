8c9d13e51d11f9b2478e525d8e6d50d0487339e9
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-22 15:24:13 -0500

    refactor: new component: imagebuilder

---
+++ src/app/lib/displayImage.tsx
@@ -0,0 +1,15 @@
+
+
+
+
+
+import { client } from "@/sanity/client";
+
+import imageUrlBuilder from "@sanity/image-url";
+
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source: SanityImageSource) {
+    return builder.image(source);
+  }
