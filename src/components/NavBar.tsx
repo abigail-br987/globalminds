c85abdee7e34fe76dbcd7416aa3c9edabd776205
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-22 21:35:48 -0500

    styles: padding and Image

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Link from "next/link";
+import Image from "next/image";
 const navItems = [
   { name: "Inicio", to: "/", style: "bg-gbGreen" },
   { name: "Programas", to: "/programas", style: "bg-gbBlue" },
@@ -13,13 +14,16 @@ function NavBar() {
     <nav className="p-2 fixed w-full top-0 z-50 font-mono">
       <ul className="items-start flex justify-between">
         <Link href={"/home"} className="cursor-pointer flex space-x-3">
-          <div className="py-1 px-3 bg-gbBlack h-max rounded-md space-x-3 font-mono font-bold flex items-center">
-            <img
+          <div className="py-1 px-2 bg-gbBlack h-max rounded-md space-x-2 font-mono font-bold flex items-center">
+            <Image
               src={"/logo/mainlogo.png"}
               alt="Home Logo"
+              layout="responsive"
+              width={50}
+              height={50}
               className="max-w-5 max-h-5 rounded-full"
-            ></img>{" "}
-            .<span>GLOBAL MINDS</span>
+            />
+            <span>GLOBAL MINDS</span>
           </div>
         </Link>
         <div className="flex space-x-2 md:space-x-4 py-1 bg-gbBlack z-0 rounded-md px-3">
