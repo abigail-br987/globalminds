da38fe241c855a0af8e18f33e771c68c8f97e946
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-22 22:02:46 -0500

    style: justify content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,13 +2,13 @@
 import SocialIcons from "./SocialIcons";
 function Footer() {
   return (
-    <div className="text-gbWhite rounded-lg w-full grid grid-cols-3 gap-3 bg-gbBlack p-6">
+    <div className="text-gbWhite rounded-lg w-full sm:grid sm:grid-cols-3 sm:gap-3 bg-gbBlack p-6">
       <div className="max-sm:hidden space-y-5">
         <h3>Contáctanos</h3>
         <p>Num +5199999999</p>
         <p>@correoelectronico</p>
       </div>
-      <div className=" space-y-5 flex flex-col items-center max-sm:w-full justify-center">
+      <div className=" space-y-5 flex flex-col text-center items-center max-sm:w-full justify-center">
         <h3>GLOBAL MINDS</h3>
         <ul className="flex space-x-5">
           <li>FAQs</li>
