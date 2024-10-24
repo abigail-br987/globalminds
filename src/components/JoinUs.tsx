be4d18ff6d85dba2954de05549004bfee44dc47d
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-24 15:38:19 -0500

    feat: add animation (Staggered)

--- src/components/JoinUs.tsx
+++ src/components/JoinUs.tsx
@@ -3,9 +3,13 @@ import { joinOptions } from "@/script/content";
 import { useState } from "react";
 import ButtonWithArrow from "./small_components/ButtonWithArrow";
 import Modal from "./Modal";
+import AnimatedDiv3 from "./small_components/AnimatedDiv3";
 import AnimatedDiv from "./small_components/AnimatedDiv";
 import ColorfulTitle from "./small_components/ColorfulTitle";
 import { globalMindsColors } from "@/script/content";
+import { useRef } from "react";
+import { useInView } from "framer-motion";
+
 interface JoinOption {
   title: string;
   icon: JSX.Element;
@@ -18,6 +22,8 @@ interface JoinOption {
 const JoinUs: React.FC = () => {
   const [modalOpen, setModalOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState<JoinOption | null>(null);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const isInView = useInView(containerRef, { once: false, margin: "-40% 0px" });
 
   const toggleModal = (option: JoinOption | null) => {
     setSelectedOption(option);
@@ -28,9 +34,11 @@ const JoinUs: React.FC = () => {
     <AnimatedDiv>
       <ColorfulTitle text="UNETE" className="m-auto"/>
       <div className="mt-6 mb-10 space-y-4 rounded text-gbWhite hyphens-auto">
-        <div className="sm:grid max-sm:space-y-8 max-sm:block grid-cols-2 gap-10">
+        <div ref={containerRef} className="sm:grid max-sm:space-y-8 max-sm:block grid-cols-2 gap-10">
           {joinOptions.map((option, index) => (
-            <div
+            <AnimatedDiv3
+            index={index}
+            isInView={isInView}
               key={index}
               className={`sm:flex text-gbBlack p-4 rounded-lg bg-${
                 globalMindsColors[index % globalMindsColors.length]
@@ -49,7 +57,7 @@ const JoinUs: React.FC = () => {
                   Más información
                 </ButtonWithArrow>
               </div>
-            </div>
+            </AnimatedDiv3>
           ))}
         </div>
       </div>
