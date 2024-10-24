c801a2dc16541db8287e19c6ae68fbc21baaf96f
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-24 07:38:48 -0500

    feat: define types and establish order

--- src/components/DetailedStructure.tsx
+++ src/components/DetailedStructure.tsx
@@ -5,27 +5,60 @@ import { globalMindsColors } from "@/script/content";
 import { urlFor } from "@/app/lib/displayImage";
 import { formatDate } from "@/app/lib/utils";
 
+interface ImageAsset {
+  _id: string;
+  url: string;
+}
+
+interface Image {
+  asset: ImageAsset;
+}
+
+interface Offering {
+  _id: string;
+  title: string;
+  description: string;
+  type: string;
+  mode?: string;
+  sociallinks?: string[];
+  register?: string;
+  emoji?: string;
+  time?: string;
+  image?: Image;
+}
+
+interface ProcessedOffering extends Offering {
+  isPast: boolean;
+  formattedDate:
+    | { day: string; month: string; hour: string; minutes: string }
+    | undefined;
+}
+
 const POSTS_QUERY = `*[ 
   _type == "offerings" 
-] | order(publishedAt asc)[0...12]{ 
-  _id, 
-  title, 
-  description, 
-  type, 
-  mode, 
-  sociallinks,
-  register, 
-  emoji, 
-  time,
-  image,
-}`;
+] | order(time asc)[0...12]`;
 
 const options = { next: { revalidate: 30 } };
 
 export default async function DetailedStructure() {
-  const elements = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
+  const elements = await client.fetch<Offering[]>(POSTS_QUERY, {}, options);
 
-  const groupedElements = elements.reduce(
+  const currentDate = new Date();
+  const processedElements: ProcessedOffering[] = elements.map((element) => {
+    const isPast = element.time ? new Date(element.time) < currentDate : false;
+    const formattedDate = element.time ? formatDate(element.time) : undefined;
+
+    return { ...element, isPast, formattedDate };
+  });
+  processedElements.sort((a, b) => {
+    if (a.isPast === b.isPast) {
+      return (
+        new Date(a.time || "").getTime() - new Date(b.time || "").getTime()
+      );
+    }
+    return a.isPast ? 1 : -1;
+  });
+  const groupedElements = processedElements.reduce(
     (acc, element) => {
       const { type } = element;
       if (!acc[type]) {
@@ -34,40 +67,42 @@ export default async function DetailedStructure() {
       acc[type].push(element);
       return acc;
     },
-    {} as Record<string, SanityDocument[]>
+    {} as Record<string, ProcessedOffering[]>
   );
-
   const uniqueTypes = Object.keys(groupedElements);
-  const currentDate = new Date();
-
-
   return (
-    <div className="container mx-auto p-4 space-y-10 text-gbBlack ">
+    <div className="container mx-auto space-y-10 text-gbBlack ">
       {uniqueTypes.map((type, index) => {
         const color = globalMindsColors[index % globalMindsColors.length];
         const columnClass =
-        type === "Mentores" ? "grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6" : "sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5";
-  
+          type === "Mentores"
+            ? "grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"
+            : "sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5";
         return (
           <div key={type} className="space-y-4">
             <h2 className="text-gbWhite">{type.toUpperCase()}</h2>
-            <div className={`grid ${columnClass} bg-${color} rounded-2xl gap-4 p-3`}>
-            {groupedElements[type].map((element) => {
-                const formattedDate = element.time ? formatDate(element.time) : undefined;
-                const isPast = element.time && new Date(element.time) < currentDate;
+            <div
+              className={`grid ${columnClass} bg-${color} rounded-2xl gap-4 p-3`}
+            >
+              {groupedElements[type].map((element) => {
                 const componentProps = {
-                  photo: element.image && element.image.asset ? String(urlFor(element.image.asset)) : undefined,
+                  photo:
+                    element.image && element.image.asset
+                      ? String(urlFor(element.image.asset))
+                      : undefined,
                   emoji: element.emoji,
                   title: element.title,
-                  formattedDate: formattedDate,
+                  formattedDate: element.formattedDate,
                   mode: element.mode,
                   type: element.type,
-                  sociallinks: element.sociallinks,
+                  sociallinks: element.sociallinks?.map((link: string) => ({
+                    platform: "unknown",
+                    url: link,
+                  })),
                   color: color,
                   description: element.description,
-                  linkName: element.linkName,
-                  url: element.url,
-                  past: isPast,
+                  url: element.register,
+                  past: element.isPast,
                 };
                 return (
                   <div key={element._id} className="">
