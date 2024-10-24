47aa54326fe2790dfec977d4f3a30a41c684856c
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-24 15:40:38 -0500

    feat: animation for entire blocks

--- src/components/small_components/AnimatedDiv.tsx
+++ src/components/small_components/AnimatedDiv.tsx
@@ -1,25 +1,36 @@
 "use client";
-import { ReactNode } from "react";
+import { useRef } from "react";
 import { motion } from "framer-motion";
-import { animations } from "../animations/animations";
-
+import { useInView } from "framer-motion";
 interface AnimatedDivProps {
-  children: ReactNode;
+  children: React.ReactNode;
   className?: string;
 }
 
-function AnimatedDiv({ children, className = "" }: AnimatedDivProps) { 
+const AnimatedDiv = ({ children, className = "" }: AnimatedDivProps) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const isInView = useInView(ref, { once: false, margin: "-40% 0px" });
+
   return (
     <motion.div
-      className={`${className}`}
-      initial="offscreen"
-      whileInView="onscreen"
-      viewport={{ once: true }}
-      variants={animations.sectionVariant}
+      ref={ref}
+      initial={{ opacity: 0.6, scale: 0.95, y: 20 }}
+      animate={{ 
+        opacity: isInView ? 1 : 0.6, 
+        scale: isInView ? 1 : 0.95, 
+        y: isInView ? 0 : 20,
+      }}
+      transition={{ 
+        duration: 0.6,
+        type: "spring",
+        stiffness: 300,
+        damping: 20,
+      }}
+      className={className}
     >
       {children}
     </motion.div>
   );
-}
+};
 
 export default AnimatedDiv;
