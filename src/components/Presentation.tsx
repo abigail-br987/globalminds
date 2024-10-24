b1516c8b920216e1c14d88f85ec5990b10085692
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-24 15:38:43 -0500

    feat: establish ref and add animation

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -1,6 +1,9 @@
 "use client";
 import Star9sides from "@/svg/Star9sides";
 import AnimatedDiv from "./small_components/AnimatedDiv";
+import AnimatedDiv2 from "./small_components/AnimatedDiv2";
+import { useRef } from "react";
+import { useInView } from "framer-motion";
 interface ContentItem {
   title: string;
   description: string;
@@ -23,43 +26,46 @@ export const missionContent: ContentItem[] = [
 ];
 
 function Presentation() {
-  return (
-    <>
-      <AnimatedDiv className="md:flex gap-6 text-gbBlack ">
-        <Star9sides className="max-sm:hidden absolute -top-20 right-0 w-32 fill-gbGreen z-10" />
-        <div className="flex-1 space-y-6">
-          {aboutUsContent.map((item, index) => (
-            <div key={index} className="bg-gbYellow p-8 rounded-lg -rotate-1">
-              <h2>{item.title}</h2>
-              <p>{item.description}</p>
-            </div>
-          ))}
-          <div>
-            <img
-              className="h-auto  max-w-full object-cover rounded-lg rotate-1 border-gbYellow border-[10px]"
-              src="https://placehold.co/600x400"
-              alt="Description"
-            />
-          </div>
-        </div>
-        <div className="flex-1 space-y-6 flex flex-col items-center justify-center">
-          <div className="max-md:hidden">
-            <img
-              className="h-auto max-w-full object-cover rounded-lg -rotate-1 border-gbGreen border-[10px]"
-              src="https://placehold.co/600x400"
-              alt="..."
-            />
-          </div>
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const isInView = useInView(containerRef, { once: false, margin: "-40% 0px" });
 
-          {missionContent.map((item, index) => (
-            <div key={index} className="bg-gbGreen p-8 rounded-lg rotate-1">
-              <h2>{item.title}</h2>
-              <p>{item.description}</p>
-            </div>
-          ))}
-        </div>
-      </AnimatedDiv>
-    </>
+
+  return (
+    <div ref={containerRef}>
+    <AnimatedDiv key={1} className="md:flex gap-6 text-gbBlack">
+      <Star9sides className="max-sm:hidden absolute -top-20 right-0 w-32 fill-gbGreen z-10" />
+      <div className="flex-1 space-y-6">
+        {aboutUsContent.map((item, index) => (
+          <div key={index} className="bg-gbYellow p-8 rounded-lg -rotate-1">
+            <h2>{item.title}</h2>
+            <p>{item.description}</p>
+          </div>
+        ))}
+        <AnimatedDiv2 isInViewContainer={isInView}>
+          <img
+            className="h-auto max-w-full object-cover rounded-lg rotate-1 border-gbYellow border-[10px]"
+            src="https://placehold.co/600x400"
+            alt="Description"
+          />
+        </AnimatedDiv2>
+      </div>
+      <div className="flex-1 space-y-6 flex flex-col items-center justify-center">
+        <AnimatedDiv2 className="max-md:hidden" isInViewContainer={isInView}>
+          <img
+            className="h-auto max-w-full object-cover rounded-lg -rotate-1 border-gbGreen border-[10px]"
+            src="https://placehold.co/600x400"
+            alt="..."
+          />
+        </AnimatedDiv2>
+        {missionContent.map((item, index) => (
+          <div key={index} className="bg-gbGreen p-8 rounded-lg rotate-1">
+            <h2>{item.title}</h2>
+            <p>{item.description}</p>
+          </div>
+        ))}
+      </div>
+    </AnimatedDiv>
+    </div>
   );
 }
 
