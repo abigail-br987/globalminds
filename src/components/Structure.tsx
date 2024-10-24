4ac25ccf43274b65cfa8d98c2e7af811eb960bb0
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-24 15:38:55 -0500

    feat: establish ref and add animations

--- src/components/Structure.tsx
+++ src/components/Structure.tsx
@@ -1,32 +1,40 @@
-"use client"
+"use client";
 import { structureItems } from "@/script/content";
 import ColorfulTitle from "./small_components/ColorfulTitle";
 import AnimatedDiv from "./small_components/AnimatedDiv";
+import AnimatedDiv3 from "./small_components/AnimatedDiv3";
+import { useRef } from "react";
+import { useInView } from "framer-motion";
+import { globalMindsColors } from "@/script/content";
+
 function Structure() {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const isInView = useInView(containerRef, { once: false, margin: "-40% 0px" });
+
   return (
-    <>
-      <AnimatedDiv className="text-center my-5  text-gbWhite flex flex-col items-center cursor-pointer">
-        <ColorfulTitle text="STRUCTURE" />
-        <div className="grid grid-cols-1 md:grid-cols-2 text-center">
-          {structureItems.map((item, index) => (
-            <div
-              className={`relative py-6 px-3 
-       ${index === 0 ? "border-b sm:border-r  " : ""} 
-       ${index === 1 ? "sm:border-b" : ""} 
-       ${index === 2 ? "sm:border-r max-sm:border-b max-sm:border-t" : ""} 
-       border-gbWhite`}
-              key={index}
-            >
-              <div className="flex items-center justify-center">
-                <h3 className="z-10">{item.title}</h3>
-                {item.figure}
-              </div>
-              <p>{item.description}</p>
+    <AnimatedDiv className="text-center my-5 text-gbWhite flex flex-col items-center cursor-pointer">
+      <ColorfulTitle text="STRUCTURE" />
+      <div ref={containerRef} className="grid grid-cols-1 md:grid-cols-2 text-center">
+        {structureItems.map((item, index) => (
+          <AnimatedDiv3
+            className={`relative py-6 px-3
+              ${index === 0 ? "border-b sm:border-r" : ""} 
+              ${index === 1 ? "sm:border-b" : ""} 
+              ${index === 2 ? "sm:border-r max-sm:border-b max-sm:border-t" : ""} 
+              border-gbWhite`}
+            key={index}
+            index={index}
+            isInView={isInView}
+          >
+            <div className="flex items-center justify-center">
+              <h3 className="z-10">{item.title}</h3>
+              {item.figure}
             </div>
-          ))}
-        </div>
-      </AnimatedDiv>
-    </>
+            <p>{item.description}</p>
+          </AnimatedDiv3>
+        ))}
+      </div>
+    </AnimatedDiv>
   );
 }
 
