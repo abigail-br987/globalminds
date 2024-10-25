12beb46ae8c5412570230e92c1afd51f1c89c366
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-24 22:53:06 -0500

    style: change padding

--- src/app/nosotros/page.tsx
+++ src/app/nosotros/page.tsx
@@ -14,7 +14,7 @@ export default async function Nosotros() {
         <Header image={banner} />
         <NavBar />
 
-        <div className="p-10">
+        <div className="p-6">
           <OurTeamServer />
         </div>
         <FollowUs />
