407b4760dd500efa95df8d662ee348e2e15dd063
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-29 09:27:45 -0500

    refactor and improve readability

--- src/components/Structure.tsx
+++ src/components/Structure.tsx
@@ -1,10 +1,54 @@
 "use client";
-import { structureItems } from "@/script/content";
 import ColorfulTitle from "./small_components/ColorfulTitle";
 import AnimatedDiv from "./small_components/AnimatedDiv";
 import AnimatedDiv3 from "./small_components/AnimatedDiv3";
 import { useRef } from "react";
 import { useInView } from "framer-motion";
+import Figure11 from "@/svg/Figure11";
+import Figure12 from "@/svg/Figure12";
+import Figure3 from "@/svg/Figure3";
+
+export const structureItems = [
+  {
+    title: "Inspirar",
+    description:
+      "Charlas semanales con latinos exitosos para inspirar a los estudiantes y ayudarles a definir sus metas.",
+    figure: (
+      <Figure3
+        className="w-10 absolute -rotate-45 z-0 fill-gbYellow"
+      />
+    ),
+  },
+  {
+    title: "Apoyar",
+    description:
+      "Mentoría personalizada para guiar a los estudiantes en sus aplicaciones a concursos, becas y universidades.",
+    figure: (
+      <Figure11
+        className="w-10 absolute -rotate-45 z-0  fill-purple-400"
+      />
+    ),
+  },
+  {
+    title: "Preparar",
+    description:
+      "Talleres y capacitaciones para fortalecer los currículums y preparar a los estudiantes para actividades relevantes.",
+    figure: (
+      <Figure12
+        color="gbBlue"
+        className="w-10 absolute -rotate-45 z-0  fill-orange-500"
+      />
+    ),
+  },
+  {
+    title: "Conectar",
+    description:
+      "Crear una comunidad de apoyo y networking entre estudiantes y mentores para fomentar la colaboración y el intercambio de experiencias.",
+    figure: <Figure12 className="w-10 absolute -rotate-45 z-0  fill-gbGreen" />,
+  },
+];
+
+
 
 function Structure() {
   const containerRef = useRef<HTMLDivElement | null>(null);
@@ -12,7 +56,9 @@ function Structure() {
 
   return (
     <AnimatedDiv className="text-center my-5 text-gbWhite flex flex-col items-center cursor-pointer">
-      <ColorfulTitle text="STRUCTURE" />
+      <span>¿Quieres saber cómo funciona Global Minds?</span> 
+      <span className="mb-5"> Tenemos una metodología de 4 pasos</span>
+      <ColorfulTitle text="METODOLOGÍA" />
       <div ref={containerRef} className="grid grid-cols-1 md:grid-cols-2 text-center">
         {structureItems.map((item, index) => (
           <AnimatedDiv3
@@ -25,8 +71,9 @@ function Structure() {
             index={index}
             isInView={isInView}
           >
-            <div className="flex items-center justify-center">
-              <h3 className="z-10">{item.title}</h3>
+            <div className="flex items-center justify-center mb-5">
+              <h3 className="z-10 font-gotaRegular" style={{ WebkitTextStroke: "2px #2B2B2B",}} // Adjust as needed
+              >{item.title}</h3>
               {item.figure}
             </div>
             <p>{item.description}</p>
