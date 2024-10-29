ca3a9a26105d0e513e78044aa32b5def95788c59
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-29 09:27:32 -0500

    feat: refactor and fix styles

--- src/components/Impact.tsx
+++ src/components/Impact.tsx
@@ -1,10 +1,29 @@
-"use client"
+"use client";
 import ColorfulTitle from "./small_components/ColorfulTitle";
-import { impactData } from "@/script/content";
 import AnimatedDiv3 from "./small_components/AnimatedDiv3";
 import AnimatedDiv from "./small_components/AnimatedDiv";
 import { useRef } from "react";
 import { useInView } from "framer-motion";
+
+export const impactData = [
+  {
+    value: "+25",
+    text: "charlas organizadas con expertos latinos",
+  },
+  {
+    value: "+1450",
+    text: "estudiantes impactados, más oportunidades",
+  },
+  {
+    value: "+35",
+    text: "mentores activos de universidades de prestigio",
+  },
+  {
+    value: "50%",
+    text: "crecimiento mensual en la participación",
+  },
+];
+
 function Impact() {
   const containerRef = useRef<HTMLDivElement | null>(null);
   const isInView = useInView(containerRef, { once: false, margin: "-40% 0px" });
@@ -12,25 +31,44 @@ function Impact() {
   return (
     <AnimatedDiv className="hyphens-auto text-gbWhite space-y-6">
       <div className="relative flex items-center justify-center w-full">
-        <ColorfulTitle text="IMPACTO"/>
+        <ColorfulTitle text="IMPACTO" />
       </div>
       <p>
-        No solo ofrecemos asesoría académica y profesional gratuita que en otros
-        lugares puede costar hasta 10,000 dólares, sino que también creamos un
+        No solo ofrecemos asesoría académica y profesional gratuita —que en
+        otros lugares puede costar hasta $10,000—, sino que también creamos un
         espacio seguro y accesible para que los jóvenes latinos sueñen en
-        grande.
+        grande. Desde su creación en 2023, Global Minds ha alcanzado resultados
+        impresionantes gracias al esfuerzo conjunto de nuestros mentees,
+        mentores y staff. A continuación, compartimos algunos de nuestros
+        logros:
       </p>
-      <div ref={containerRef} className=" sm:grid max-sm:space-y-6 sm:grid-cols-2 md:grid-cols-3 sm:gap-6 justify-items-center text-gbBlack">
+      <div
+        ref={containerRef}
+        className=" sm:grid items-center max-sm:space-y-6 sm:grid-cols-2 md:grid-cols-3 sm:gap-6 justify-items-center text-gbBlack"
+      >
         {impactData.map((item, index) => (
-          <AnimatedDiv3 key={index} isInView={isInView} index={index} className="bg-gbYellow p-4 rounded-lg text-center">
-            <h3 className="">
-              <span className="text-7xl">{item.value}</span> {item.text}
+          <AnimatedDiv3
+            key={index}
+            isInView={isInView}
+            index={index}
+            className="bg-gbYellow flex p-4 rounded-lg text-center"
+          >
+            <h3 className="text-7xl">
+            {item.value}
             </h3>
+            <span>{item.text}</span>
           </AnimatedDiv3>
         ))}
+        <div className="col-span-2 text-gbWhite">
+          Cada cifra cuenta una historia; detrás de estos números están las
+          vidas de jóvenes latinos que están alcanzando sus sueños académicos y
+          profesionales. Nuestro compromiso es seguir creciendo, llegar a más
+          estudiantes y expandir el impacto de nuestra red global.
+        </div>
         {/*
         <button className="bg-gbWhite text-gbBlack h-max self-center py-4 rounded-xl"> <p> Leer Testimonios </p><FaReadme className="text-3xl m-auto"/> </button>
-      */} </div>
+      */}{" "}
+      </div>
     </AnimatedDiv>
   );
 }
