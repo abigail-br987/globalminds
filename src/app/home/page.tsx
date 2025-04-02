b13b12d0ae7e730f44799796b746c432152fc9a6
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-02 17:36:37 -0500

    paths

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,4 +1,4 @@
-import Landing from "@/components/Landing";
+import Landing from "@/components/inicio/Landing";
 import Structure from "@/components/Structure";
 import Photos from "@/components/Photos";
 import Figures from "@/components/Figures";
@@ -6,14 +6,11 @@ import JoinUs from "@/components/JoinUs";
 import Donate from "@/components/Donate";
 import Impact from "@/components/Impact";
 import Footer from "@/components/Footer";
-import Presentation from "@/components/Presentation";
-
+import Presentation from "@/components/inicio/Presentation";
 
 function Home() {
   return (
     <div className="flex flex-col justify-center items-center m-auto bg-gbBlack">
-
-
       {/*  <Landing />
 
     <div className="max-w-screen-xl p-10 z-10 space-y-10">
