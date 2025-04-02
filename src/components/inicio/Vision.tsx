1a644fa78f2c5c6806cdc257977a14af4b93fb39
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-02 17:37:31 -0500

    refactor vision

---
+++ src/components/inicio/Vision.tsx
@@ -0,0 +1,17 @@
+function Vision() {
+  return (
+    <div className="space-y-4">
+      <h2>VISIÓN</h2>
+      <div className="border-gbGreen border-4 p-8 rounded-lg">
+        <p>
+          Nuestra misión es crear una plataforma donde los jóvenes puedan
+          acceder a mentores, charlas inspiradoras y talleres prácticos,
+          eliminando barreras económicas y culturales que limitan las
+          oportunidades de muchos.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Vision;
