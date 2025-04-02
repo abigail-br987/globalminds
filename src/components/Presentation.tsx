1b3dca172a94b6073030996bd3bc2250ada2ad8a
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-02 16:34:14 -0500

    refactor

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -11,7 +11,7 @@ interface ContentItem {
 
 export const aboutUsContent: ContentItem[] = [
   {
-    title: "¿Quiénes Somos?",
+    title: "¿QUIENES SOMOS?",
     description:
       "Somos una organización juvenil que empodera jóvenes latinos, proporcionando acceso a recursos, conexiones y mentoría personalizada. Creemos que el éxito individual puede multiplicarse para el beneficio de la comunidad, por lo que ofrecemos asesoría gratuita para apoyar a los estudiantes a lograr sus metas educativas y profesionales en el extranjero.",
   },
@@ -19,7 +19,15 @@ export const aboutUsContent: ContentItem[] = [
 
 export const missionContent: ContentItem[] = [
   {
-    title: "Nuestra Misión",
+    title: "MISIÓN",
+    description:
+      "Nuestra misión es crear una plataforma donde los jóvenes puedan acceder a mentores, charlas inspiradoras y talleres prácticos, eliminando barreras económicas y culturales que limitan las oportunidades de muchos.",
+  },
+];
+
+export const visionContent: ContentItem[] = [
+  {
+    title: "VISIÓN",
     description:
       "Nuestra misión es crear una plataforma donde los jóvenes puedan acceder a mentores, charlas inspiradoras y talleres prácticos, eliminando barreras económicas y culturales que limitan las oportunidades de muchos.",
   },
@@ -29,42 +37,51 @@ function Presentation() {
   const containerRef = useRef<HTMLDivElement | null>(null);
   const isInView = useInView(containerRef, { once: false, margin: "-40% 0px" });
 
-
   return (
     <div ref={containerRef}>
-    <AnimatedDiv key={1} className="md:flex gap-6 text-gbBlack">
-      <Star9sides className="max-sm:hidden absolute -top-20 right-0 w-32 fill-gbGreen z-10" />
-      <div className="flex-1 space-y-6">
+      <AnimatedDiv key={1} className="gap-6 ">
+        <Star9sides className="max-sm:hidden absolute -top-20 right-0 w-32 fill-gbGreen z-10" />
+
         {aboutUsContent.map((item, index) => (
-          <div key={index} className="bg-gbYellow p-8 rounded-lg -rotate-1">
-            <h2>{item.title}</h2>
-            <p>{item.description}</p>
-          </div>
+          <>
+            <h2 className="text-gbWhite tracking-widest">{item.title}</h2>
+            <div
+              key={index}
+              className="bg-gbYellow p-8 text-gbBlack rounded-lg"
+            >
+              <p>{item.description}</p>
+            </div>
+          </>
         ))}
-        <AnimatedDiv2 isInViewContainer={isInView}>
-          <img
-            className="h-auto max-w-full object-cover rounded-lg rotate-1 border-gbYellow border-[10px]"
-            src="https://placehold.co/600x400"
-            alt="Description"
-          />
-        </AnimatedDiv2>
-      </div>
-      <div className="flex-1 space-y-6 flex flex-col items-center justify-center">
-        <AnimatedDiv2 className="max-md:hidden" isInViewContainer={isInView}>
-          <img
-            className="h-auto max-w-full object-cover rounded-lg -rotate-1 border-gbGreen border-[10px]"
-            src="https://placehold.co/600x400"
-            alt="..."
-          />
-        </AnimatedDiv2>
-        {missionContent.map((item, index) => (
-          <div key={index} className="bg-gbGreen p-8 rounded-lg rotate-1">
-            <h2>{item.title}</h2>
-            <p>{item.description}</p>
+
+        <div className="flex-1 flex items-center justify-center text-center gap-6 my-10">
+        <div>
+          {missionContent.map((item, index) => (
+            <>
+              <h2 className="tracking-widest mb-5">{item.title}</h2>
+
+              <div key={index} className="border-gbRed border-4 p-8 rounded-lg">
+                <p>{item.description}</p>
+              </div>
+              </>
+          ))}
           </div>
-        ))}
-      </div>
-    </AnimatedDiv>
+          <div>
+          {visionContent.map((item, index) => (
+            <>
+              <h2 className="tracking-widest mb-5">{item.title}</h2>
+
+              <div
+                key={index}
+                className="border-gbGreen border-4 p-8 rounded-lg"
+              >
+                <p>{item.description}</p>
+              </div>
+            </>
+          ))}
+          </div>
+        </div>
+      </AnimatedDiv>
     </div>
   );
 }
