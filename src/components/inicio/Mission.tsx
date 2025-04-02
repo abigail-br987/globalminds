8d81ec78d4e3deb493585f27c4a045abb31a3edd
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-02 17:37:16 -0500

    refactor mission

---
+++ src/components/inicio/Mission.tsx
@@ -0,0 +1,17 @@
+function Mission() {
+  return (
+    <div className="space-y-4">
+      <h2>MISIÓN</h2>
+      <div className="border-gbRed border-4 p-8 rounded-lg">
+        <p>
+          Nuestra misión es crear una plataforma donde los jóvenes puedan
+          acceder a mentores, charlas inspiradoras y talleres prácticos,
+          eliminando barreras económicas y culturales que limitan las
+          oportunidades de muchos.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Mission;
