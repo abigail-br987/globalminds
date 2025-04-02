2ef7caf260bcd163d94ee025ab89267e18280030
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-02 17:37:22 -0500

    refactor presentation

---
+++ src/components/inicio/Presentation.tsx
@@ -0,0 +1,20 @@
+"use client";
+function Presentation() {
+  return (
+    <div className="space-y-4">
+      <h2>¿QUIENES SOMOS?</h2>
+      <div className="bg-gbYellow p-8 text-gbBlack rounded-lg">
+        <p>
+          Somos una organización juvenil que empodera jóvenes latinos,
+          proporcionando acceso a recursos, conexiones y mentoría personalizada.
+          Creemos que el éxito individual puede multiplicarse para el beneficio
+          de la comunidad, por lo que ofrecemos asesoría gratuita para apoyar a
+          los estudiantes a lograr sus metas educativas y profesionales en el
+          extranjero.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Presentation;
