159779929a08f9eb46159a078fd4f74b57b48e34
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-02 20:01:34 -0500

    path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Landing from "@/components/inicio/Landing";
 import Structure from "@/components/inicio/Structure";
-import Figures from "@/components/Figures";
+import Figures from "@/components/figures/Figures";
 import JoinUs from "@/components/inicio/JoinUs";
-import Donate from "@/components/Donate";
+import Donate from "@/components/inicio/Donate";
 import Impact from "@/components/inicio/Impact";
 import Presentation from "@/components/inicio/Presentation";
 import Mission from "@/components/inicio/Mission";
@@ -16,8 +16,8 @@ function Home() {
       <div className="max-w-screen-md m-auto p-10 space-y-20">
         <Presentation />
         <div className="flex text-center gap-6">
-          <Mission/>
-          <Vision/>
+          <Mission />
+          <Vision />
         </div>
         <Structure />
         <Impact />
