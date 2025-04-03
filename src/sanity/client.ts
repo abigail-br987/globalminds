9e410e074b58fb662f07e9aaeb7cdccae1954dff
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-03 07:18:25 -0500

    remove connection to sanity

--- src/sanity/client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "next-sanity";
-
-export const client = createClient({
-  projectId: "tjyia6xp",
-  dataset: "production",
-  apiVersion: "2024-01-01",
-  useCdn: false,
-});
