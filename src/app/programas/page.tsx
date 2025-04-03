804039cdf0162022b63e08531ce6bae4c5e990a9
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-03 07:19:31 -0500

    remove connection to sanity

--- src/app/programas/page.tsx
+++ src/app/programas/page.tsx
@@ -1,16 +1,8 @@
-import NavBar from "@/components/navigation/NavBar";
-import Footer from "@/components/Footer";
 import Header from "@/components/small_components/Header";
-import DetailedStructureServer from "@/components/programs/DetailedStructureServer";
 function Programas() {
   return (
     <div className="flex flex-col justify-center items-center m-auto bg-gbBlack">
-      <NavBar />
       <Header image={"/images/banner.png"}></Header>
-      <div className="p-6 mt-4">
-        <DetailedStructureServer />
-      </div>
-      <Footer />
     </div>
   );
 }
