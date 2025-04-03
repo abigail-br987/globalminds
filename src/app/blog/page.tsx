bb06a3812801f56d400ee4816ce54f5d6fb4eb37
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-04-03 07:18:53 -0500

    clean page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,19 +1,6 @@
-import Landing from "@/components/inicio/Landing";
-import Structure from "@/components/inicio/Structure";
-import Figures from "@/components/figures/Figures";
-import JoinUs from "@/components/inicio/JoinUs";
-import Donate from "@/components/inicio/Donate";
-import Impact from "@/components/inicio/Impact";
-import Presentation from "@/components/inicio/Presentation";
-import Mission from "@/components/inicio/Mission";
-import Vision from "@/components/inicio/Vision";
-
 export default async function IndexPage() {
   return (
     <div>
-      <NavBar />
-      <BlogServer />
-      <Footer />
     </div>
   );
 }
